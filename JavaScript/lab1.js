fc95776df3329ac4d5d029479be7537ababb94fd
KlevSuper <106628323+KlevSuper@users.noreply.github.com>
2023-06-03 10:15:28 +0300

    1

--- JavaScript/lab1.js
+++ JavaScript/lab1.js
@@ -2,20 +2,20 @@
 function vozrast_pol() {
     let age,gender;
 age = prompt('Сколько вам лет?');
-gender = prompt('Вы мужчина или женщина?');
-if ((age>=0 && age <= 17)&&(gender=='мужчина' || gender=='женщина')){
+gender = prompt('Вы мужчина или женщина? (или "1" и "2" соответственно)');
+if ((age>=0 && age <= 17)&&(gender=='мужчина' || (gender=='женщина' && gender=='2'))){
     alert('Вам работать ещё рано — учитесь!');
-}  else if ((age>=18 && age <= 59) && gender=='мужчина'){
+}  else if ((age>=18 && age <= 59) && (gender=='мужчина' && gender=='1')){
     alert('Вам ещё работать и работать!');
-} else if ((age>=18 && age <= 54) && gender=='женщина'){
+} else if ((age>=18 && age <= 54) && (gender=='женщина' && gender=='2')){
     alert('Вам ещё работать и работать!');
-}else if ((age>=60 && age < 65) && gender=='мужчина'){
+}else if ((age>=60 && age < 65) && (gender=='мужчина' && gender=='1')){
     alert('Скоро пенсия!');
-} else if ((age>=55 && age < 60) && gender=='женщина'){
+} else if ((age>=55 && age < 60) && (gender=='женщина' && gender=='2')){
     alert('Скоро пенсия!');
-}  else if (age >= 65 && gender=='мужчина'){
+}  else if (age >= 65 && (gender=='мужчина' && gender=='1')){
     alert('Вам пора на пенсию!');
-} else if (age >= 60 && gender=='женщина'){
+} else if (age >= 60 && (gender=='женщина' && gender=='2')){
     alert('Вам пора на пенсию!');
 }  else {
     alert('Да кто ты такой?')
